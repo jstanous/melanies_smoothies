8beef9a94dd4bf1205f522679cad076c898bda64
jstanous <jstanous@outlook.com>
2025-07-22 11:29:04 -0700

    Update streamlit_app.py
    Testing Pandas search

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,9 +37,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      # add pandas search to get SEARCH_ON value
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       # new scetion to display nutrition information
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
