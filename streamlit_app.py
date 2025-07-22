4a3c2004b5f17a3457217e5bf9a38a864c2e0911
jstanous <jstanous@outlook.com>
2025-07-22 10:50:58 -0700

    Update streamlit_app.py
    Add dataframe for nutrition data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,12 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # new scetion to display nutrition information
-import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Title and subtitle
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
