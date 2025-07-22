07efd899159dc2b9f49ccd277b847910477b1779
jstanous <jstanous@outlook.com>
2025-07-22 10:52:58 -0700

    Update streamlit_app.py
    Remove nutrition write

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 
 # new scetion to display nutrition information
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Title and subtitle
