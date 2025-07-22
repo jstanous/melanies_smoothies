7f323072e724f638093b7a1a17190a91b4646bb3
jstanous <jstanous@outlook.com>
2025-07-22 10:56:53 -0700

    Update streamlit_app.py
    Move nutrition info to ingredients loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      # new scetion to display nutrition information
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     # st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients, name_on_order)
@@ -43,7 +47,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon='✅')
 
-# new scetion to display nutrition information
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
